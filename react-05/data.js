a5df1ec7c095ac5f136c143e83868105d0297f98
Mikie <2047441719@qq.com>
2018-12-20 22:10:43 +0800

    'react学习小结'

---
+++ react-05/data.js
@@ -0,0 +1,48 @@
+const dataList = {
+    "colleagues":{
+        "name":"同事",
+        "list":[
+            {
+                id:Math.random(),
+                "name":'Tom',
+                "gender":'male',
+                "introduce":'He like man'
+            },
+            {
+                id:Math.random(),
+                "name":'Sun',
+                "gender":'male',
+                "introduce":'He like red flower'
+            },
+            {
+                id:Math.random(),
+                "name":'Mark',
+                "gender":'male',
+                "introduce":'He comes from U.S.A'
+            },
+            {
+                id:Math.random(),
+                "name":'Marny',
+                "gender":'female',
+                "introduce":"She hasn't boyfriend"
+            },
+        ]
+    },
+    "lovers":{
+        "name":"情人",
+        "list":[
+            {
+                id:Math.random(),
+                "name":'Han',
+                "gender":'male',
+                "introduce":'White skin'
+            },
+            {
+                id:Math.random(),
+                "name":'Kary',
+                "gender":'female',
+                "introduce":'Long hair'
+            },
+        ]
+    }
+}
